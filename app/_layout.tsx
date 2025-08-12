916e6b3d30bcf31468db37bfa72409c4f067c740
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-12 18:36:48 +0500

    refactor: implented tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
-import { Stack } from "expo-router";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <ThemeProvider value={DefaultTheme}>
+        <StatusBar style="auto" />
+        <Tabs screenOptions={{ tabBarActiveTintColor: "teal" }}>
+            <Tabs.Screen 
+                name="index"
+                options={{
+                    title: "Camera",
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="photo-camera" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tabs.Screen 
+                name="saved-palettes"
+                options={{
+                    title: "Saved Palettes",
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="palette" size={size} color={color} />
+                    )
+                }}
+            />
+        </Tabs>
+      </ThemeProvider>
+  );
 }
