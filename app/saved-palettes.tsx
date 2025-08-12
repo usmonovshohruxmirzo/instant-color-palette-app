5db6711bc3bf34fdc4f9e0e93915aef7c0bb7507
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-12 18:36:11 +0500

    feat: add saved-palettes screen

---
+++ app/saved-palettes.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function SavedPalletesScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Text>Saved Palettes Screen</Text>
+    </View>
+  );
+}
+
